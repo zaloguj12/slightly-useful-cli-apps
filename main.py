cc9900403ba5d99e509b7b179864e428155027b6
Zaloguj12 <zaloguj12@proton.me>
2026-07-10 21:16:54 +0200

    add calc func, add gitingore

---
+++ main.py
@@ -0,0 +1,51 @@
+import scripts.calculator
+import random
+import math
+
+
+def calc(operation_calc):
+    if operation_calc == "+":
+        print("enter your number then enter your second number")
+        num1 = input("First number: ")
+        num2 = input("Second number: ")
+        result = scripts.calculator.add(num1, num2)
+        print(f"Your result is: {result}")
+    elif operation_calc == "-":
+        print("enter your number then enter your second number")
+        num1 = input("First number: ")
+        num2 = input("Second number: ")
+        result = scripts.calculator.min(num1, num2)
+        print(f"Your result is: {result}")
+    elif operation_calc == "*":
+        print("enter your number then enter your second number")
+        num1 = input("First number: ")
+        num2 = input("Second number: ")
+        result = scripts.calculator.mult(num1, num2)
+        print(f"Your result is: {result}")
+    elif operation_calc == "/":
+        print("enter your number then enter your second number")
+        num1 = input("First number: ")
+        num2 = input("Second number: ")
+        result = scripts.calculator.div(num1, num2)
+        print(f"Your result is: {result}")
+
+
+
+
+while True:
+    selected_option = input()
+    operation_calc = "null"
+    if selected_option == "help":
+        print("Here is a list of avaiable commands:")
+        print("note")
+        print("calc")
+        print("color")
+        print("unit")
+        print("help")
+        print("exit")
+    elif selected_option == "exit":
+        break
+    elif selected_option == "calc":
+        print("What operation?")
+        print("+ - * /")
+        calc(input())
---
+++ scripts/calculator.py
@@ -0,0 +1,17 @@
+import math
+
+def add(num1, num2):
+    result = int(num1) + int(num2)
+    return result
+
+def min(num1, num2):
+    result = int(num1) - int(num2)
+    return result
+
+def mult(num1, num2):
+    result = int(num1) * int(num2)
+    return result
+
+def div(num1, num2):
+    result = int(num1) / int(num2)
+    return result
